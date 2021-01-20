7d2e59e16df9db0825f96fcf5d1f204cb8a713bf
Maglione Riccardo <riccardo99maglione@gmail.com>
2021-01-20 19:00:32 +0100

    Adjust formatting of PlayerController

--- Assets/Gameplay/Scripts/PlayerController.cs
+++ Assets/Gameplay/Scripts/PlayerController.cs
@@ -2,114 +2,140 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour
+namespace SwordGame
 {
-
-    //gravity of fall
-
-    #region Variables
-    float waitTime;
-    [Header("Movement Value")]
-    [Tooltip("It's an acceleration of player")]
-    public float Acceleration;
-    [Tooltip("It's a velocity of player on right and left way")]
-    public float Speed;
-    [Tooltip("It's a max speed of player")]
-    public float MaxSpeed;
-    [Header("Jump Value")]
-    [Tooltip("It's a force of player's jump")]
-    public float jumpForce;
-    [Tooltip("It's a time of change rotation offset of platform")]
-    public float TimeDoublePlatform;
-    [Tooltip("")]
-    public float fallMultiplier = 2.5f;
-    [Tooltip("")]
-    public float lowJumpMultiplier = 2f;
-
-    float tempSpeed;
-    bool Grounded = true;
-    Rigidbody2D rb;
-    GameObject TempPlatform;
-    #endregion
-
-    void Start()
+    [System.Serializable]
+    public struct StructMovement
     {
-        rb = GetComponent<Rigidbody2D>();
-        waitTime = TimeDoublePlatform;
-        tempSpeed = Speed;
+        [Tooltip("It's an acceleration of player")]
+        public float Acceleration;
+        [Tooltip("It's a velocity of player on right and left way")]
+        public float Speed;
+        [Tooltip("It's a max speed of player")]
+        public float MaxSpeed;
     }
-
-    void Update()
+    
+    [System.Serializable]
+    public struct StructJump
     {
-        if (Input.GetKeyDown(KeyCode.A))
+        [Tooltip("It's a force of player's jump")]
+        public float jumpForce;
+        [Tooltip("It's value of gravity fall")]
+        public float fallMultiplier;
+        [Tooltip("It's a value for progressive jump")]
+        public float lowJumpMultiplier;
+    }
+    
+    [RequireComponent(typeof(BoxCollider2D))]
+    [RequireComponent(typeof(Rigidbody2D))]
+
+    public class PlayerController : MonoBehaviour
+    {
+        #region Variables
+        public StructMovement ValueMovement;
+        public StructJump ValueJump;
+
+        bool Grounded = true;
+        float waitTime;
+        float tempSpeed;
+        [Tooltip("It's a time of change rotation offset of platform")]
+        public float TimeDoublePlatform;
+        
+        Rigidbody2D rb;
+        GameObject TempPlatform;
+        #endregion
+
+        void Start()
         {
-            Speed = tempSpeed;
-        }
-        else if (Input.GetKeyDown(KeyCode.D))
-        {
-            Speed = tempSpeed;
+            rb = GetComponent<Rigidbody2D>();
+            waitTime = TimeDoublePlatform;
+            tempSpeed = ValueMovement.Speed;
         }
 
+        void Update()
+        {
+            PlayerMovement();
+            PlayerJump();
+            ResetPlatform();
+        }
 
+        #region Player - Move and Jump
+        public void PlayerMovement()
+        {
+            if (Input.GetKeyDown(KeyCode.A))
+            {
+                ValueMovement.Speed = tempSpeed;
+            }
+            else if (Input.GetKeyDown(KeyCode.D))
+            {
+                ValueMovement.Speed = tempSpeed;
+            }
 
-        if (Input.GetKey(KeyCode.A))
-        {
-            Speed = Speed + Acceleration * Time.deltaTime;
-            if (Speed >= MaxSpeed)
-                Speed = MaxSpeed;
-            print(Speed);
-            rb.velocity = new Vector2(-Speed, rb.velocity.y);
+            if (Input.GetKey(KeyCode.A))
+            {
+                CalculateSpeed();
+                rb.velocity = new Vector2(-ValueMovement.Speed, rb.velocity.y);
+            }
+            else if (Input.GetKey(KeyCode.D))
+            {
+                CalculateSpeed();
+                rb.velocity = new Vector2(ValueMovement.Speed, rb.velocity.y);
+            }
+            else
+            {
+                rb.velocity = new Vector2(0, rb.velocity.y);
+            }
         }
-        else if (Input.GetKey(KeyCode.D))
+        public void PlayerJump()
         {
-            Speed = Speed + Acceleration * Time.deltaTime;
-            if (Speed >= MaxSpeed)
-                Speed = MaxSpeed;
-            print(Speed);
-            rb.velocity = new Vector2(Speed + Acceleration * Time.deltaTime, rb.velocity.y);
+            if (Input.GetKey(KeyCode.Space) && Grounded == true && rb.velocity.y == 0)
+            {
+                rb.AddForce(Vector2.up * ValueJump.jumpForce, ForceMode2D.Impulse);
+                Grounded = false;
+            }
+            if (rb.velocity.y < 0)
+            {
+                rb.velocity += Vector2.up * Physics2D.gravity.y * (ValueJump.fallMultiplier - 1) * Time.deltaTime;
+            }
+            else if (rb.velocity.y > 0 && !Input.GetButton("Jump"))
+            {
+                rb.velocity += Vector2.up * Physics2D.gravity.y * (ValueJump.lowJumpMultiplier - 1) * Time.deltaTime;
+            }
         }
-        else
+        public void CalculateSpeed()
         {
-            rb.velocity = new Vector2(0, rb.velocity.y);
+            ValueMovement.Speed = ValueMovement.Speed + ValueMovement.Acceleration * Time.deltaTime;
+            if (ValueMovement.Speed >= ValueMovement.MaxSpeed)
+                ValueMovement.Speed = ValueMovement.MaxSpeed;
         }
-        #region Jump Section
-        if (Input.GetKey(KeyCode.Space) && Grounded == true && rb.velocity.y == 0)
+        public void ResetPlatform()
         {
-            rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
-            Grounded = false;
-        }
-        if (rb.velocity.y < 0)
-        {
-            rb.velocity += Vector2.up * Physics2D.gravity.y * (fallMultiplier - 1) * Time.deltaTime;
-        }
-        else if (rb.velocity.y > 0 && !Input.GetButton("Jump"))
-        {
-            rb.velocity += Vector2.up * Physics2D.gravity.y * (lowJumpMultiplier - 1) * Time.deltaTime;
+            if (TempPlatform != null)
+            {
+                waitTime -= Time.deltaTime;
+                if (waitTime <= 0)
+                {
+                    TempPlatform.GetComponent<PlatformEffector2D>().rotationalOffset = 0;
+                    TempPlatform = null;
+                    waitTime = TimeDoublePlatform;
+                }
+            }
         }
         #endregion
 
-        if (TempPlatform != null)
+        #region Collision
+        private void OnCollisionEnter2D(Collision2D collision)
         {
-            waitTime -= Time.deltaTime;
-            if (waitTime <= 0)
+            Grounded = true;
+        }
+        private void OnCollisionStay2D(Collision2D collision)
+        {
+            if (Input.GetKey(KeyCode.S))
             {
-                TempPlatform.GetComponent<PlatformEffector2D>().rotationalOffset = 0;
-                TempPlatform = null;
-                waitTime = TimeDoublePlatform;
+                collision.gameObject.GetComponent<PlatformEffector2D>().rotationalOffset = 180;
+                TempPlatform = collision.gameObject;
             }
         }
-    }
-
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
-        Grounded = true;
-    }
-    private void OnCollisionStay2D(Collision2D collision)
-    {
-        if (Input.GetKey(KeyCode.S))
-        {
-            collision.gameObject.GetComponent<PlatformEffector2D>().rotationalOffset = 180;
-            TempPlatform = collision.gameObject;
-        }
+        #endregion
     }
 }
