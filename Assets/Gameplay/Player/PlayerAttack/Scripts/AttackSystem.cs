ae963d5f60591339c89108e58b74dd74385e8246
leolaz99 <leolaz99@gmail.com>
2021-01-26 18:27:28 +0100

    added attack system

---
+++ Assets/Gameplay/Player/PlayerAttack/Scripts/AttackSystem.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AttackSystem : MonoBehaviour
+{
+    [Tooltip("Velocità attacco")]
+    [SerializeField] float attackTimer;
+    
+    PlayerInput playerInput;   
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("HIT");
+    }
+
+    IEnumerator Attack()
+    {
+        yield return new WaitForSeconds(attackTimer);
+        playerInput.isAttack = false;
+        gameObject.SetActive(false);       
+    }
+
+
+    void Awake()
+    {
+        playerInput = FindObjectOfType<PlayerInput>();
+    }
+
+    void OnEnable()
+    {
+        StartCoroutine(Attack());
+    }
+}
---
+++ Assets/Gameplay/Player/PlayerAttack/Scripts/PlayerInput.cs
@@ -0,0 +1,75 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerInput : MonoBehaviour
+{
+    //velocita del giocatore
+    //[SerializeField] float speed;
+
+    //velocita originale del giocatore
+    //float originalSpeed;
+
+    [SerializeField] GameObject lightAttackCollider;
+    [SerializeField] GameObject heavyAttackCollider;
+    [SerializeField] GameObject specialAttackCollider;
+    
+    [Header("KEYBOARD INPUTS")]
+    [SerializeField] KeyCode KeyboardLightlAttack;
+    [SerializeField] KeyCode KeyboardHeavyAttack;
+    [SerializeField] KeyCode KeyboardSpecialAttack;
+
+    [HideInInspector] public bool isAttack = false;
+
+    Rigidbody rb;
+ 
+    void LightAttack()
+    {
+        lightAttackCollider.SetActive(true);
+        isAttack = true;
+    }
+
+    void HeavyAttack()
+    {
+        heavyAttackCollider.SetActive(true);
+        isAttack = true;
+    }
+
+    void SpecialAttack()
+    {
+        specialAttackCollider.SetActive(true);
+        isAttack = true;
+    }
+
+
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyboardLightlAttack) && isAttack == false)
+        {
+            //originalSpeed = speed;
+            //speed = 0;
+            LightAttack();
+            //speed = originalSpeed;
+        }
+
+        if (Input.GetKeyDown(KeyboardHeavyAttack) && isAttack == false)
+        {
+            //originalSpeed = speed;
+            //speed = 0;
+            HeavyAttack();
+            //speed = originalSpeed;
+        }
+
+        if (Input.GetKeyDown(KeyboardSpecialAttack) && isAttack == false)
+        {
+            //originalSpeed = speed;
+            //speed = 0;
+            SpecialAttack();
+            //speed = originalSpeed;
+        }
+    }
+}
