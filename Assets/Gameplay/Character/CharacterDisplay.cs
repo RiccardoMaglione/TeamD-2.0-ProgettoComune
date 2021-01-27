d1bb45f06f8ef20df551ffde335e32c1271a1e1e
Maglione Riccardo <riccardo99maglione@gmail.com>
2021-01-27 13:51:12 +0100

    Add scriptable object for character and state machine base with override

---
+++ Assets/Gameplay/Character/CharacterDisplay.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterDisplay : MonoBehaviour
+{
+    public CharacterManager CM;
+
+    public string Name;
+    public int Life;
+    public int Energy;
+    public RuntimeAnimatorController RAC;
+    public AnimatorOverrideController AOC;
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.K))
+        {
+            InitializeCharacter();
+        }
+    }
+
+    public void InitializeCharacter()
+    {
+        Name = CM.PlayerName;
+        Life = CM.PlayerLife;
+        Energy = CM.PlayerEnergy;
+        GetComponent<SpriteRenderer>().sprite = CM.PlayerSprite;
+        RAC = CM.PlayerBaseAnimator;
+        AOC = CM.PlayerOverrideAnimator;
+        GetComponent<Animator>().runtimeAnimatorController = CM.PlayerOverrideAnimator;
+    }
+}
---
+++ Assets/Gameplay/Character/CharacterManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Player", menuName = "Character")]
+public class CharacterManager : ScriptableObject
+{
+    [Header("When the character is player")]
+    public string PlayerName;
+    public int PlayerID;
+    public Sprite PlayerSprite;
+    public int PlayerLife;
+    public int PlayerEnergy;
+
+    public float PlayerAcceleration;
+    public float PlayerSpeed;
+    public float PlayerMaxSpeed;
+
+    public float PlayerJumpForce;
+    public float PlayerFallMultiplier;
+    public float PlayerLowJumpMultiplier;
+
+    public bool PlayerHasDashEffect;
+
+    public RuntimeAnimatorController PlayerBaseAnimator;
+    public AnimatorOverrideController PlayerOverrideAnimator;
+
+    [Header("When the character is enemy")]
+    public string EnemyName;
+    public int EnemyID;
+    public Sprite EnemySprite;
+    public int EnemyLife;
+    public int EnemyEnergy;
+
+    public float EnemyAcceleration;
+    public float EnemySpeed;
+    public float EnemyMaxSpeed;
+}
