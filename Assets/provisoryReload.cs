0c2140d81a01e15f59ab15d2e6a14a2a1f1f56e0
Andrea Facchinetti <facchiandrea1510@gmail.com>
2021-02-02 10:23:59 +0100

    Reload scene + sprite for player attack

---
+++ Assets/provisoryReload.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class provisoryReload : MonoBehaviour
+{
+    public void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+}
