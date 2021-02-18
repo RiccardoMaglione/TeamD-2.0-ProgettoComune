199806ed58ad34961f7113940f4b6b6f595df18c
leolaz99 <leolaz99@gmail.com>
2021-02-18 18:24:23 +0100

    added 2 boss attack

--- Assets/Boss/Scripts/WaypointsAttack1.cs
+++ Assets/Boss/Scripts/WaypointsAttack1.cs
@@ -6,20 +6,18 @@ public class WaypointsAttack1 : MonoBehaviour
     int i = 0;
     public float speed;
     float WPradius = 0.1f;
-    [SerializeField] Animator anim;
 
     public void Attack1()
     {
-        if (waypoints.Length == 0)
-            i = 0;
-        
-        if (i >= waypoints.Length)
-            anim.SetBool("FinishAttack", true);
-        
         if ((Vector3.Distance(waypoints[i].transform.position, transform.position) < WPradius) && i < waypoints.Length)
         {
             i++;
         }
         transform.position = Vector3.MoveTowards(transform.position, waypoints[i].transform.position, Time.deltaTime * speed);       
+    }
+
+    void Update()
+    {
+        Attack1();
     }
 }
--- Assets/Boss/Scripts/WaypointsAttack2.cs
+++ Assets/Boss/Scripts/WaypointsAttack2.cs
@@ -5,21 +5,19 @@ public class WaypointsAttack2 : MonoBehaviour
     public GameObject[] waypoints;
     int i = 0;
     public float speed;
-    float WPradius = 1;
-    [SerializeField] Animator anim;
+    float WPradius = 0.1f;
 
     public void Attack2()
     {
-        if (waypoints.Length == 0)
-            i = 0;
-        
-        if (i >= waypoints.Length)
-            anim.SetBool("FinishAttack", true);
-        
         if ((Vector3.Distance(waypoints[i].transform.position, transform.position) < WPradius) && i < waypoints.Length)
         {
             i++;
         }
         transform.position = Vector3.MoveTowards(transform.position, waypoints[i].transform.position, Time.deltaTime * speed);       
+    }
+
+    void Update()
+    {
+        Attack2();
     }
 }
