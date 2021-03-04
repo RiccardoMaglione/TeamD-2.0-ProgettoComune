cc92240bbcbcd587433e46d73b6b99f8c4325fb9
jacopo_saibene <jacopo.scuola0@gmail.com>
2021-03-04 19:30:02 +0100

    Added Crossbow

---
+++ Assets/Gameplay/Traps/CrossBow/Scripts/Bullet.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    private Transform target;
+    public float speed = 4f;
+
+    private void Start()
+    {
+        target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+    }
+
+    void shotDirection()
+    {
+        if (Vector3.Distance(transform.position, target.position) > 0f) ///1
+        {
+            transform.position = Vector3.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
+        }
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            Destroy(this.gameObject); //sistemare la collisione e la direzione dello sparo
+        }
+    }
+
+    private void Update()
+    {
+        shotDirection();
+    }
+}
