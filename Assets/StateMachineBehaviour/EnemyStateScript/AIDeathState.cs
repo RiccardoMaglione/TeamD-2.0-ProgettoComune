347ca508268ebcc6ed2dcbd4af42ca3dcfd33030
Andrea Facchinetti <facchiandrea1510@gmail.com>
2021-03-07 21:53:50 +0100

    Update scripts

--- Assets/StateMachineBehaviour/EnemyStateScript/AIDeathState.cs
+++ Assets/StateMachineBehaviour/EnemyStateScript/AIDeathState.cs
@@ -1,34 +1,31 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using SwordGame;
+﻿using UnityEngine;
 
 public class AIDeathState : StateMachineBehaviour
 {
     // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
     override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-        /* if(FindObjectOfType<ScoreSystem>(true).SpecialType == true)
-        {
-            FindObjectOfType<ScoreSystem>(true).ScoreAssignedEnemyDestroy((int)animator.GetComponent<EnemyData>().TypeEnemy,3);
-        }
-        else
-        {
-            FindObjectOfType<ScoreSystem>(true).ScoreAssignedEnemyDestroy((int)animator.GetComponent<EnemyData>().TypeEnemy, 3);
-        } */
+        //if(FindObjectOfType<ScoreSystem>(true).SpecialType == true)
+        //{
+        //    FindObjectOfType<ScoreSystem>(true).ScoreAssignedEnemyDestroy((int)animator.GetComponent<EnemyData>().TypeEnemy,3);
+        //}
+        //else
+        //{
+        //    FindObjectOfType<ScoreSystem>(true).ScoreAssignedEnemyDestroy((int)animator.GetComponent<EnemyData>().TypeEnemy, 3);
+        //}
         animator.GetComponent<EnemyData>().gameObject.SetActive(false);
     }
 
     // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
     //override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     //{
-    //    
+    //
     //}
 
     // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
     //override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     //{
-    //    
+    //
     //}
 
     // OnStateMove is called right after Animator.OnAnimatorMove()
