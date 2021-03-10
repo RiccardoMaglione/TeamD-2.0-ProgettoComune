b3fb205ad3915142f4bb78a228c41f336db3070f
Andrea Facchinetti <facchiandrea1510@gmail.com>
2021-03-10 01:45:01 +0100

    Add script WIP

--- Assets/BookFlip/PageFlipper.cs
+++ Assets/BookFlip/PageFlipper.cs
@@ -1,18 +1,52 @@
 ﻿using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PageFlipper : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField]
+    private GameObject page1Pivot;
+    [SerializeField]
+    private GameObject page2Pivot;
+    [SerializeField]
+    private GameObject page3Pivot;
+    [SerializeField]
+    private GameObject page4Pivot;
+    [SerializeField]
+    private GameObject page5Pivot;
+    [SerializeField]
+    private GameObject page6Pivot;
+
+    private Quaternion notFlippedPosition;
+    private Quaternion flippedPosition;
+
+    private int pageCounter;
+
+    [SerializeField]
+    private float flipSpeed;
+
+    private void Start()
     {
-        
+        pageCounter = 0;
+        notFlippedPosition = Quaternion.Euler(0, 0, 0);
+        flippedPosition = Quaternion.Euler(0, 180, 0);
+    }
+    /*private void Update()
+    {
+        page1Pivot.transform.rotation = Quaternion.Lerp(flippedPosition, notFlippedPosition, Time.time * flipSpeed);
+
+    }*/
+    public void Page1FlipForward()
+    {
+        StartCoroutine(Page1FlipForwardCoroutine());
+    }
+    public void Page1FlipFBackward()
+    {
     }
 
-    // Update is called once per frame
-    void Update()
+    IEnumerator Page1FlipForwardCoroutine()
     {
-        
+        while (pageCounter < 2)
+            page1Pivot.transform.rotation = Quaternion.Lerp(flippedPosition, notFlippedPosition, Time.time * flipSpeed);
+        yield return null;
     }
 }
