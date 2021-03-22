9254ae140652f7919cb4128c852166df84ca7662
leolaz99 <leolaz99@gmail.com>
2021-03-22 16:14:34 +0100

    added score system

---
+++ Assets/Gameplay/Scripts/ScoreManager.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+
+public class ScoreManager : MonoBehaviour
+{
+    public float enemyKillPoints = 0;
+    public float bossKillPoints = 0;
+    public float possessionPoints = 0;
+    public float specialAttackActivationPoints = 0;
+    public float skullCollectPoints = 0;
+    public float lightAttackPoints = 0;
+    public float heavyAttackPoints = 0;
+
+    [ReadOnly]
+    public float totatPoints;
+
+    public void AddEnemyKillPoints()
+    {
+        totatPoints = totatPoints + enemyKillPoints * ScoreMultiplierManager.instance.actualMultiplierValue;
+    }
+
+    public void AddBossKillPoints()
+    {
+        totatPoints = totatPoints + bossKillPoints * ScoreMultiplierManager.instance.actualMultiplierValue;
+    }
+
+    public void AddPossessionPoints()
+    {
+        totatPoints = totatPoints + possessionPoints * ScoreMultiplierManager.instance.actualMultiplierValue;
+    }
+
+    public void AddSpecialAttackActivationPoints()
+    {
+        totatPoints = totatPoints + specialAttackActivationPoints * ScoreMultiplierManager.instance.actualMultiplierValue;
+    }
+
+    public void AddSkullCollectionPoints()
+    {
+        totatPoints = totatPoints + skullCollectPoints * ScoreMultiplierManager.instance.actualMultiplierValue;
+    }
+
+    public void AddLightAttackPoints()
+    {
+        totatPoints = totatPoints + lightAttackPoints * ScoreMultiplierManager.instance.actualMultiplierValue;
+    }
+
+    public void AddHeavyAttackPoints()
+    {
+        totatPoints = totatPoints + heavyAttackPoints * ScoreMultiplierManager.instance.actualMultiplierValue;
+    }
+}
---
+++ Assets/Gameplay/Scripts/ScoreMultiplierManager.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+
+public class ScoreMultiplierManager : MonoBehaviour
+{
+    public static ScoreMultiplierManager instance;
+
+    [Header("NUMERO DI NEMICI PER ATTIVARE OGNI MOLTIPLICATORE")]
+    
+    [ReadOnly]
+    public float enemyDeathCount = 0;
+    public int enemyMultiplier1Count = 0;
+    public int enemyMultiplier2Count = 0;
+    public int enemyMultiplier3Count = 0;
+    public int enemyMultiplier4Count = 0;
+    public int enemyMultiplier5Count = 0;
+
+    [Header("MULTIPLIERS VALUES")]
+    public float multiplier1Value = 0;
+    public float multiplier2Value = 0;
+    public float multiplier3Value = 0;
+    public float multiplier4Value = 0;
+    public float multiplier5Value = 0;
+
+    [ReadOnly]
+    public float actualMultiplierValue = 1;
+
+    void Awake()
+    {
+        if (instance == null)
+            instance = this;
+    }
+
+    void Update()
+    {
+        if (enemyDeathCount >= enemyMultiplier5Count)
+            actualMultiplierValue = multiplier5Value;
+
+        else
+        if(enemyDeathCount >= enemyMultiplier4Count)
+            actualMultiplierValue = multiplier4Value;
+
+        else
+        if (enemyDeathCount >= enemyMultiplier3Count)
+            actualMultiplierValue = multiplier3Value;
+
+        else
+        if (enemyDeathCount >= enemyMultiplier2Count)
+            actualMultiplierValue = multiplier2Value;
+
+        else
+        if (enemyDeathCount >= enemyMultiplier1Count)
+            actualMultiplierValue = multiplier1Value;
+
+        else
+        if (enemyDeathCount < enemyMultiplier1Count)
+            actualMultiplierValue = 1;
+    }
+}
