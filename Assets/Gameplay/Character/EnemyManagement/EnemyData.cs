6391971357c5d0038afb88af128bee0bc5772459
Andrea Facchinetti <facchiandrea1510@gmail.com>
2021-03-25 19:08:59 +0100

    ParticleTest

--- Assets/Gameplay/Character/EnemyManagement/EnemyData.cs
+++ Assets/Gameplay/Character/EnemyManagement/EnemyData.cs
@@ -1,42 +1,45 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using UnityEngine;
 
 public class EnemyData : MonoBehaviour
 {
     #region Variables
+
     #region Life Behaviour / Stun Sub-Behaviour - Possession Sub-Behaviour
     public TypeEnemies TypeEnemy;
+    public ParticleSystem bloodPS;
+
     [Space(10)]
     [Header("------------------------ Life Behaviour -----------------------------------------------------------------------------------------------------------------------------")]
     [Tooltip("Indica la vita del player")]
     public int Life;                                                     //life behaviour, indica la vita del player
-    
+
     [Header("   ►  First Method - From stun to destoy")]
     [Header("~~~ Stun Sub-Behaviour ~~~ Value Management")]
     [Space(10)]
     [ReadOnly] public float timerStun = 0;                               //stun behaviour o life behaviour, è il timer di stun - readonly
     [Tooltip("Indica la durata dello stato di stun prima che il personaggio si distrugga")]
     public float DurationStun = 5;                                       //stun behaviour o life behaviour, max value of timer di stun 
-    
+
     [Header("   ►  Second Method - From stun to destoy")]
     [ReadOnly] public int CountHit = 0;                                  //Stunned behaviour quindi potremmo metterlo nel life behaviour, indica quanti colpi ha dato il player al nemico dopo lo stun - readonly
     [Tooltip("Indica quanti attacchi da parte del player servano prima che il personaggio si distrugga")]
     public int MaxCountHit = 0;                                          //Stunned behaviour quindi potremmo metterlo nel life behaviour, indica il max dei colpi prima di scomparire
     [Header("   ►  Boolean for know if state stun is active or not - ReadOnly")]
-    [ReadOnly]public bool isStun = false;
-    
+    [ReadOnly] public bool isStun = false;
+
     [Header("   ►  Boolean for the know if the player can possess this enemy - ReadOnly")]
     [Header("~~~ Possesion Sub-Behaviour ~~~")]
     [Space(10)]
     [ReadOnly] public bool isPossessed = false;                          //Behaviour? forse life o stun, indica se l'enemy può essere posseduto oppure no
-    
+
     [Space(10)]
     [Header("~~~ Stagger Sub-Behaviour ~~~")]
     [Header("   ►  Count of max number of hit for activate stagger")]
     [Space(20)]
     public int CountPoiseEnemy;
     public int MaxCountPoiseEnemy;
+
     #endregion
 
     #region Movement Behaviour
@@ -50,19 +53,19 @@ public class EnemyData : MonoBehaviour
     public List<GameObject> WaypointEnemy = new List<GameObject>();                                    //Movement Behaviour Lista di waypoint per il path
 
     [ReadOnly] public int WaypointIndex;                                  //Movement Behaviour - Indice del waypoint = readonly
-    
+
     [Space(10)]
     [Header("►  Value for indicate if the player can move")]
     [Tooltip("Condition - Standard: True - 'Temp false for ranged'")]
     public bool CanMove = true;                                           //Indica se il player si può muovere - si potrebbe anche usare se un nemico non si può muovere
-    
+
     [Space(10)]
     [Header("►  Variables for control when the player is in aggro")]
     [ReadOnly] public GameObject PlayerEnemy;                             //Movement Behaviour e Attack Behaviour - nel movement serve per andare in contro al player e nell'attack serve per la distance per il reset
-    
+
     [ReadOnly] public bool CanVisible = false;                            //Movement Bahaviour e Attack Behaviour - Indica se il nemico viene visto (quindi se entra nel collider trigger) - readonly
     #endregion
-    
+
     #region Variables Attack Behaviour
     [Space(10)]
     [Header("►  Collider Attack - Light And Heavy")]
@@ -72,7 +75,8 @@ public class EnemyData : MonoBehaviour
 
     [Space(10)]
     [Header("►  Percentuage of spawn of collider attack - Light And Heavy")]
-    [Tooltip("Da 0 a Percentuage corrisponde al light attack, da percentuage a 100 è heavy attack\n0 <= Percentuage = Light && Percentuage >= 100 = Heavy")][Range(0,100)]
+    [Tooltip("Da 0 a Percentuage corrisponde al light attack, da percentuage a 100 è heavy attack\n0 <= Percentuage = Light && Percentuage >= 100 = Heavy")]
+    [Range(0, 100)]
     public int PercentuageAttack;
     [ReadOnly] public int random;                                          //Attack Bahaviour - Variabile per la pesca random per poi confrontare con la percentuale
     [Tooltip("Sarebbe la variabile che indica che il random può attivarsi")]
@@ -87,7 +91,7 @@ public class EnemyData : MonoBehaviour
     [Space(10)]
     [Header("►  Reset Cycle Attack - Value Management - ReadOnly")]
     [ReadOnly] public bool CanReset = false;                                //Resetta i timer e i booleani di attacco quando il player esce fuori dall'aggro, visto che lo resettava ad ogni attaco, è stato strutturato tramite il controllo della distanza
-    
+
 
     [Space(10)]
     [Header("~~~ AI Animation - State Machine Behaviour - Speed Management ~~~ ")]
@@ -196,7 +200,7 @@ public class EnemyData : MonoBehaviour
         ColliderAttack.SetActive(false);
     }
     #endregion
-    
+
     #endregion
 
     #region Trigger Zone
