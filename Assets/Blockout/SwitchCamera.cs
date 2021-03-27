e8207a4d6dec3249e13129109b0f027b04bf319e
Andrea Facchinetti <facchiandrea1510@gmail.com>
2021-03-27 19:04:21 +0100

    Edit colliders size + add aggro spawn on trigger camera

--- Assets/Blockout/SwitchCamera.cs
+++ Assets/Blockout/SwitchCamera.cs
@@ -3,7 +3,11 @@
 public class SwitchCamera : MonoBehaviour
 {
     public GameObject nextCamera;
+    public GameObject nextConfiner;
     public GameObject currentCamera;
+    public GameObject currentConfiner;
+
+    public GameObject activateAggroBox1;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
@@ -12,6 +16,13 @@ public class SwitchCamera : MonoBehaviour
             nextCamera.SetActive(true);
             this.gameObject.SetActive(false);
             currentCamera.SetActive(false);
+            nextConfiner.SetActive(true);
+            currentConfiner.SetActive(false);
+            if (activateAggroBox1 != null)
+            {
+                activateAggroBox1.SetActive(true);
+            }
+
         }
     }
 
