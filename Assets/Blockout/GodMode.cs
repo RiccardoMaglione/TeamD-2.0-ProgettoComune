e4058cf38d9ebf07a72342eae02a1a234fc2c282
Andrea Facchinetti <facchiandrea1510@gmail.com>
2021-03-27 23:30:24 +0100

    fix GodMode

--- Assets/Blockout/GodMode.cs
+++ Assets/Blockout/GodMode.cs
@@ -5,12 +5,14 @@ namespace SwordGame
     public class GodMode : MonoBehaviour
     {
         public GameObject player;
+        public HealthBar healthBar;
 
         public bool godModeActive = false;
 
         public void SetGodMode()
         {
             player.GetComponent<PSMController>().MaxHealth = 1000000;
+            player.GetComponent<PSMController>().CurrentHealth = 1000000;
 
         }
     }
