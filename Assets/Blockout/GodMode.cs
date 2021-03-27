d2822baab2f81108e3a9c1235af299841960ec75
Andrea Facchinetti <facchiandrea1510@gmail.com>
2021-03-27 23:12:32 +0100

    Added godmode (blue button)

---
+++ Assets/Blockout/GodMode.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+namespace SwordGame
+{
+    public class GodMode : MonoBehaviour
+    {
+        public GameObject player;
+
+        public bool godModeActive = false;
+
+        public void SetGodMode()
+        {
+            player.GetComponent<PSMController>().MaxHealth = 1000000;
+
+        }
+    }
+
+}
