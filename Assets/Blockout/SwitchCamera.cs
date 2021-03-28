80420cf5c82c40429547b2efa662af4fe48a7acf
Andrea Facchinetti <facchiandrea1510@gmail.com>
2021-03-28 20:02:11 +0200

    Confiner edit

--- Assets/Blockout/SwitchCamera.cs
+++ Assets/Blockout/SwitchCamera.cs
@@ -29,4 +29,22 @@ public class SwitchCamera : MonoBehaviour
         }
     }
 
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player") && killedEnemyToProgress == FindObjectOfType<KilledEnemyCounter>().killedEnemyCounter)
+        {
+            nextCamera.SetActive(true);
+            this.gameObject.SetActive(false);
+            currentCamera.SetActive(false);
+            nextConfiner.SetActive(true);
+            currentConfiner.SetActive(false);
+            if (activateAggroBox1 != null)
+            {
+                activateAggroBox1.SetActive(true);
+            }
+
+        }
+    }
+
+
 }
