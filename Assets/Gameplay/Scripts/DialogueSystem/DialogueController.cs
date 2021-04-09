e33a5359ef15bd5e6c0815ceb83ca74ac21ccd50
leolaz99 <leolaz99@gmail.com>
2021-04-09 18:59:48 +0200

    added dialogues

---
+++ Assets/Gameplay/Scripts/DialogueSystem/DialogueController.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueController : MonoBehaviour
+{
+    void Update()
+    {
+        if (DialogueManager.instance.isTalk)
+            if (Input.anyKeyDown)
+                DialogueManager.instance.DisplayNextSentence();
+    }
+}
