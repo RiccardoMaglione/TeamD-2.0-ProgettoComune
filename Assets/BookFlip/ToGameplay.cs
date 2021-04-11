ed5049c031ace6806d781244c1fb10cfd5208b05
Andrea Facchinetti <facchiandrea1510@gmail.com>
2021-04-11 21:04:58 +0200

    Link bookflip to level

---
+++ Assets/BookFlip/ToGameplay.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ToGameplay : MonoBehaviour
+{
+    public void ToGameplayButton()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
