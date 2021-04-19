3a3acee26a682352eda5ea9ab75265a49afa686c
Maglione Riccardo <riccardo99maglione@gmail.com>
2021-04-19 12:03:45 +0200

    Deactive collider attack when player die

--- Assets/PSMDie.cs
+++ Assets/PSMDie.cs
@@ -1,12 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using SwordGame;
 
 public class PSMDie : StateMachineBehaviour
 {
     // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
     override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
+        animator.GetComponent<PSMController>().LightAttackCollider.SetActive(false);
+        animator.GetComponent<PSMController>().HeavyAttackCollider.SetActive(false);
         //Destroy(animator.gameObject);
     }
 
