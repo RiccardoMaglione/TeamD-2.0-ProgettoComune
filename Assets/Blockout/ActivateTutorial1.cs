0c4f08b450a0329ce81c06529c195538873da8bb
Andrea Facchinetti <facchiandrea1510@gmail.com>
2021-04-22 17:50:38 +0200

    Transitions done

---
+++ Assets/Blockout/ActivateTutorial1.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class ActivateTutorial1 : MonoBehaviour
+{
+    public GameObject tutorial1Trigger;
+
+    private void Start()
+    {
+        Invoke("ActiveTutorial1", 0.5f);
+    }
+
+    public void ActiveTutorial1()
+    {
+        tutorial1Trigger.SetActive(true);
+    }
+}
---
+++ Assets/Blockout/ScriptDelay.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class ScriptDelay : MonoBehaviour
+{
+    private void Start()
+    {
+        Invoke("EnableAfterDelay", 0.5f);
+    }
+
+    private void EnableAfterDelay()
+    {
+        this.gameObject.GetComponent<DialogueType1>().enabled = true;
+    }
+}
