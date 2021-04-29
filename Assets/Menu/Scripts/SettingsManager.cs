7437ce2b403a7414f07f5157a7738cfd446fe6d7
leolaz99 <leolaz99@gmail.com>
2021-04-29 16:44:15 +0200

    added window mode and slider on pause menu

---
+++ Assets/Menu/Scripts/SettingsManager.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class SettingsManager : MonoBehaviour
+{
+    [SerializeField] Toggle toggle;
+
+    public void SetWindow()
+    {
+        if (toggle.isOn == false)
+        {
+            Screen.fullScreen = true;
+            PlayerPrefs.SetInt("Fullscreen", 1);
+        }
+
+        if (toggle.isOn == true)
+        {
+            Screen.fullScreen = false;
+            PlayerPrefs.SetInt("Fullscreen", 0);
+        }
+    }
+
+    void Awake()
+    {
+        if (PlayerPrefs.GetInt("Fullscreen") == 0)
+            toggle.isOn = true;
+        else
+            toggle.isOn = false;
+    }
+}
