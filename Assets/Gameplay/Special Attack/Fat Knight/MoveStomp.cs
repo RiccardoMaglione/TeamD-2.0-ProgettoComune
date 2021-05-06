2080593ebe9f8ac66274b53f5aaa8c85ad6c3fb8
leolaz99 <leolaz99@gmail.com>
2021-05-06 14:27:00 +0200

    added fat knight special attack

---
+++ Assets/Gameplay/Special Attack/Fat Knight/MoveStomp.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+public class MoveStomp : MonoBehaviour
+{
+    SpecialAttack specialAttack;
+    [SerializeField] float speed;
+    Vector2 direction;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Enemy"))
+            direction = new Vector2(0, 1);
+    }
+
+    private void Awake()
+    {
+        direction = new Vector2(0, -1);
+        specialAttack = FindObjectOfType<SpecialAttack>();
+    }
+
+    void Update()
+    {
+        transform.Translate(direction * speed * Time.deltaTime);
+        if(transform.position.y >= 11)
+        {
+            Destroy(gameObject);
+            specialAttack.i++;
+            specialAttack.animator.SetTrigger("Repeat");
+        }
+
+        if(transform.position.y <= specialAttack.obj[specialAttack.i].transform.position.y)
+            direction = new Vector2(0, 1);
+
+        //transform.position = new Vector2(specialAttack.obj[specialAttack.i].transform.position.x, transform.position.y);
+    }
+}
