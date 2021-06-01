6149f0224cb614caab16eaa673bf80512ac9d7e4
Maglione Riccardo <riccardo99maglione@gmail.com>
2021-06-01 10:44:46 +0200

    Added prefab circle wipe for transition between level scene and gameover

---
+++ Assets/Daemons Love & Carnage/Gameplay/Feature/GameOver/CircleWipe.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CircleWipe : MonoBehaviour
+{
+    public RectTransform RT;
+    public float timer;
+    public float speed;
+
+    void Start()
+    {
+        InitializeScaleCircle();
+    }
+
+    void Update()
+    {
+        ScaleCircle();
+    }
+
+    public void InitializeScaleCircle()
+    {
+        RT = GetComponent<RectTransform>();
+        RT.sizeDelta = new Vector2(1100, 1100);
+    }
+
+    public void ScaleCircle()
+    {
+        if(RT.sizeDelta.x >= 0 && RT.sizeDelta.y >= 0)
+        {
+            timer += Time.deltaTime;
+            RT.sizeDelta = new Vector2(1100 - timer * speed, 1100 - timer * speed);
+        }
+    }
+}
