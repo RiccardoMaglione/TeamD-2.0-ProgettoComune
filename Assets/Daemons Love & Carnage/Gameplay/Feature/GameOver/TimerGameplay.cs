1b49b2d338d3abf4a4ebef4a7cc16f27a8e1737f
Maglione Riccardo <riccardo99maglione@gmail.com>
2021-06-02 11:55:18 +0200

    Added gameplay timer

---
+++ Assets/Daemons Love & Carnage/Gameplay/Feature/GameOver/TimerGameplay.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimerGameplay : MonoBehaviour
+{
+    public float Timer;
+
+    public float Seconds;
+    public int Minutes;
+    public int Hours;
+
+    public string HoursZero;
+    public float speedTimer;
+    void Update()
+    {
+        TimeGame();
+    }
+
+    public void TimeGame()
+    {
+        Seconds += Time.deltaTime * speedTimer;
+        Timer += Time.deltaTime;
+        if(Seconds > 59)
+        {
+            Seconds = 0;
+            Minutes++;
+            if(Minutes > 59)
+            {
+                Minutes = 0;
+                Hours++;
+            }
+        }
+
+        if (Hours <= 9)
+        {
+            HoursZero = "0";
+        }
+        else
+        {
+            HoursZero = "";
+        }
+
+        print(HoursZero + Hours + ":" + Minutes.ToString("00") + ":" + Seconds.ToString("00"));
+
+        PlayerPrefs.SetString("StringTimerGameplay", HoursZero + Hours + ":" + Minutes.ToString("00") + ":" + Seconds.ToString("00"));
+
+        PlayerPrefs.SetFloat("TimerGameplay", Timer);
+    }
+}
