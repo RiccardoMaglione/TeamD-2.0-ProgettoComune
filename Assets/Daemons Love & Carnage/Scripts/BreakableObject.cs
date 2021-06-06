4eac10cef71078b256dbae69c5427a63cc633ead
Andrea Facchinetti <facchiandrea1510@gmail.com>
2021-06-06 21:33:10 +0200

    Fix killedenemycounter on props

--- Assets/Daemons Love & Carnage/Scripts/BreakableObject.cs
+++ Assets/Daemons Love & Carnage/Scripts/BreakableObject.cs
@@ -6,7 +6,6 @@ public class BreakableObject : MonoBehaviour
     [SerializeField] private float HitCounter = 0;
     [SerializeField] GameObject Pieces;
 
-    public bool enemyCounterIncreased = false;
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.GetComponentInParent<PSMController>().IsLightAttack == true)
@@ -14,17 +13,11 @@ public class BreakableObject : MonoBehaviour
             HitCounter++;
             if (HitCounter >= 3)
             {
-                if (enemyCounterIncreased == false)
-                {
-                    FindObjectOfType<KilledEnemyCounter>().killedEnemyCounter++;
-                    enemyCounterIncreased = true;
-                }
 
                 this.gameObject.SetActive(false);
                 Pieces.SetActive(true);
                 this.gameObject.GetComponent<BoxCollider2D>().isTrigger = false;
                 this.gameObject.AddComponent<Rigidbody2D>();
-                //Debug.Log("Manichino Mortucciso");
             }
         }
         if (collision.GetComponentInParent<PSMController>().IsHeavyAttack == true)
@@ -32,17 +25,11 @@ public class BreakableObject : MonoBehaviour
             HitCounter++;
             if (HitCounter >= 3)
             {
-                if (enemyCounterIncreased == false)
-                {
-                    FindObjectOfType<KilledEnemyCounter>().killedEnemyCounter++;
-                    enemyCounterIncreased = true;
-                }
 
                 this.gameObject.SetActive(false);
                 Pieces.SetActive(true);
                 this.gameObject.GetComponent<BoxCollider2D>().isTrigger = false;
                 this.gameObject.AddComponent<Rigidbody2D>();
-                //Debug.Log("ManichinoMortucciso");
             }
         }
         if (collision.GetComponentInParent<PSMController>().IsSpecialAttack == true)
@@ -50,17 +37,11 @@ public class BreakableObject : MonoBehaviour
             HitCounter++;
             if (HitCounter >= 3)
             {
-                if (enemyCounterIncreased == false)
-                {
-                    FindObjectOfType<KilledEnemyCounter>().killedEnemyCounter++;
-                    enemyCounterIncreased = true;
-                }
 
                 this.gameObject.SetActive(false);
                 Pieces.SetActive(true);
                 this.gameObject.GetComponent<BoxCollider2D>().isTrigger = false;
                 this.gameObject.AddComponent<Rigidbody2D>();
-                Debug.Log("ManichinoMortucciso");
             }
         }
 
