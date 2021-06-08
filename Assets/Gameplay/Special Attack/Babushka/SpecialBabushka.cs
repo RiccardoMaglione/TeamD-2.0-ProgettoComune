d41611813ddd4c1e84a99c0e6367e2cf0b499e47
Francesco Guida <fraguieh@gmail.com>
2021-06-08 10:15:03 +0200

    Fixed frames to the Special Babushka

--- Assets/Gameplay/Special Attack/Babushka/SpecialBabushka.cs
+++ Assets/Gameplay/Special Attack/Babushka/SpecialBabushka.cs
@@ -10,6 +10,19 @@ public class SpecialBabushka : MonoBehaviour
     [SerializeField] GameObject attack1;
     [SerializeField] GameObject attack2;
 
+    #region Speed Animation Special
+    public float SpecialPlayerIdleSpeed = 1;
+    public float SpecialPlayerMoveSpeed = 1;
+    public float SpecialPlayerDashSpeed = 1;
+    public float SpecialPlayerFallSpeed = 1;
+    public float SpecialPlayerDashFallSpeed = 1;
+    public float SpecialPlayerJumpSpeed = 1;
+    public float SpecialPlayerDieSpeed = 1;
+    public float SpecialPlayerStaggerSpeed = 1;
+    public float SpecialPlayerLightAttackSpeed = 1;
+    public float SpecialPlayerHeavyAttackSpeed = 1;
+    public float SpecialPlayerSpecialAttackSpeed = 1;
+    #endregion
 
 
     public AnimatorOverrideController OriginalBabushkaOverride;
@@ -25,6 +38,7 @@ public class SpecialBabushka : MonoBehaviour
         GetComponentInParent<PSMController>().LightAttackCollider = attack1;
         GetComponentInParent<PSMController>().HeavyAttackCollider = attack2;
         animator.GetComponentInParent<PSMController>().GetComponent<Animator>().SetBool("PSM-SpecialAttack", false);
+        InitializeSpeedAnimationSpecial();
         yield return new WaitForSeconds(time);
         GetComponentInParent<PSMController>().LightAttackCollider = originalLight;
         GetComponentInParent<PSMController>().HeavyAttackCollider = originalHeavy;
@@ -33,5 +47,19 @@ public class SpecialBabushka : MonoBehaviour
 
         GetComponentInParent<PSMController>().GetComponent<Animator>().runtimeAnimatorController = OriginalBabushkaOverride;
         GetComponentInParent<PSMController>().InitializeSpeedAnimation();
+    }
+    public void InitializeSpeedAnimationSpecial()
+    {
+        GetComponentInParent<PSMController>().GetComponent<Animator>().SetFloat("PlayerIdleSpeed", SpecialPlayerIdleSpeed);
+        GetComponentInParent<PSMController>().GetComponent<Animator>().SetFloat("PlayerMoveSpeed", SpecialPlayerMoveSpeed);
+        GetComponentInParent<PSMController>().GetComponent<Animator>().SetFloat("PlayerDashSpeed", SpecialPlayerDashSpeed);
+        GetComponentInParent<PSMController>().GetComponent<Animator>().SetFloat("PlayerFallSpeed", SpecialPlayerFallSpeed);
+        GetComponentInParent<PSMController>().GetComponent<Animator>().SetFloat("PlayerDashFallSpeed", SpecialPlayerDashFallSpeed);
+        GetComponentInParent<PSMController>().GetComponent<Animator>().SetFloat("PlayerJumpSpeed", SpecialPlayerJumpSpeed);
+        GetComponentInParent<PSMController>().GetComponent<Animator>().SetFloat("PlayerDieSpeed", SpecialPlayerDieSpeed);
+        GetComponentInParent<PSMController>().GetComponent<Animator>().SetFloat("PlayerStaggerSpeed", SpecialPlayerStaggerSpeed);
+        GetComponentInParent<PSMController>().GetComponent<Animator>().SetFloat("PlayerLightAttackSpeed", SpecialPlayerLightAttackSpeed);
+        GetComponentInParent<PSMController>().GetComponent<Animator>().SetFloat("PlayerHeavyAttackSpeed", SpecialPlayerHeavyAttackSpeed);
+        GetComponentInParent<PSMController>().GetComponent<Animator>().SetFloat("PlayerSpecialAttackSpeed", SpecialPlayerSpecialAttackSpeed);
     }
 }
