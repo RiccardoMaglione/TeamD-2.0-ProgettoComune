10adf24a6d4ae86492a14295dc89ec2b02f31827
Andrea Facchinetti <facchiandrea1510@gmail.com>
2021-06-09 15:07:29 +0200

    gitflow-feature-stash: Skull

---
+++ Assets/SkullScript.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+public class SkullScript : MonoBehaviour
+{
+    [SerializeField] GameObject interactionButtonIcon;
+    [SerializeField] ParticleSystem ps;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            interactionButtonIcon.SetActive(true);
+        }
+    }
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                this.gameObject.SetActive(false);
+                Instantiate(ps, this.gameObject.transform.position, this.gameObject.transform.rotation);
+
+                //inserire nelle playerpref modifica alla pagina di conferma livello + impostare il teschio come ottenuto e attivabile nel menu teschi
+            }
+        }
+
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            interactionButtonIcon.SetActive(false);
+        }
+
+    }
+}
