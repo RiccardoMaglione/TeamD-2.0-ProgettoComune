58fb78c085497d860918b4a1e04343222a994486
Maglione Riccardo <riccardo99maglione@gmail.com>
2021-06-13 14:01:43 +0200

    Add key bind in main menu

---
+++ Assets/Daemons Love & Carnage/Gameplay/Feature/KeyBind/KeyBinding.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+
+public class KeyBinding : KeyVar
+{
+    public static KeyBinding KeyBindInstance;
+
+    public static Func<string, KeyCode> KeyBindSet;
+    public static Action<Button> SetListener;
+
+    [HideInInspector] public Text KeyText;
+    [HideInInspector] public bool ActivateGetKey;
+
+    Button TempButton;
+
+    private void Awake()
+    {
+        GetStringKeyPrefs();
+
+        KeyBindInstance = this;
+
+        KeyBindSet = SetKeyBind;
+        SetListener = ListenerOnClik;
+
+        SetAddListener();
+    }
+    private void Update()
+    {
+        ButtonKeyListener();
+        AssignStringContainer();
+    }
+
+    public void ListenerOnClik(Button ButtonKey)
+    {
+        ActivateGetKey = true;
+        KeyText = ButtonKey.GetComponentInChildren<Text>();
+        TempButton = ButtonKey;
+        KeyText.text = "None";
+    }
+    public void SetAddListener()
+    {
+        if (KeyUp != null)
+            KeyUp.onClick.AddListener(delegate { ListenerOnClik(KeyUp); });
+        if (KeyDown != null)
+            KeyDown.onClick.AddListener(delegate { ListenerOnClik(KeyDown); });
+        if (KeyLeft != null)
+            KeyLeft.onClick.AddListener(delegate { ListenerOnClik(KeyLeft); });
+        if (KeyRight != null)
+            KeyRight.onClick.AddListener(delegate { ListenerOnClik(KeyRight); });
+        if (KeyDash != null)
+            KeyDash.onClick.AddListener(delegate { ListenerOnClik(KeyDash); });
+        if (KeyPossession != null)
+            KeyPossession.onClick.AddListener(delegate { ListenerOnClik(KeyPossession); });
+        if (KeyLightAttack != null)
+            KeyLightAttack.onClick.AddListener(delegate { ListenerOnClik(KeyLightAttack); });
+        if (KeyHeavyAttack != null)
+            KeyHeavyAttack.onClick.AddListener(delegate { ListenerOnClik(KeyHeavyAttack); });
+        if (KeySpecialAttack != null)
+            KeySpecialAttack.onClick.AddListener(delegate { ListenerOnClik(KeySpecialAttack); });
+    }
+    public void ButtonKeyListener()
+    {
+        if (ActivateGetKey == true)
+        {
+            foreach (KeyCode vKey in Enum.GetValues(typeof(KeyCode)))
+            {
+                if (Input.GetKey(vKey))
+                {
+                    ActivateGetKey = false;
+                    KeyText.text = vKey.ToString();
+                    PlayerPrefs.SetString(TempButton.name.ToString(), KeyText.text);
+                }
+            }
+        }
+    }
+    public KeyCode SetKeyBind(string KeyValue)
+    {
+        if(ActivateGetKey == false)
+        {
+            KeyCode NewKey = (KeyCode)Enum.Parse(typeof(KeyCode), KeyValue);
+            print(NewKey.ToString());
+            return NewKey;
+        }
+        return KeyCode.None;
+    }
+}
---
+++ Assets/Daemons Love & Carnage/Gameplay/Feature/KeyBind/KeyVar.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class KeyVar : MonoBehaviour
+{
+    /*[HideInInspector]*/ public string StringKeyUp;
+    /*[HideInInspector]*/ public string StringKeyDown;
+    /*[HideInInspector]*/ public string StringKeyLeft;
+    /*[HideInInspector]*/ public string StringKeyRight;
+    /*[HideInInspector]*/ public string StringKeyDash;
+    /*[HideInInspector]*/ public string StringKeyPossession;
+    /*[HideInInspector]*/ public string StringKeyLightAttack;
+    /*[HideInInspector]*/ public string StringKeyHeavyAttack;
+    /*[HideInInspector]*/ public string StringKeySpecialAttack;
+
+    public Button KeyUp;
+    public Button KeyDown;
+    public Button KeyLeft;
+    public Button KeyRight;
+    public Button KeyDash;
+    public Button KeyPossession;
+    public Button KeyLightAttack;
+    public Button KeyHeavyAttack;
+    public Button KeySpecialAttack;
+
+    /// <summary>
+    /// Funzione generalizzata che assegna a una variabile la stringa di testo corrispondente del pulsante
+    /// </summary>
+    /// <param name="KeyString"></param>
+    /// <param name="KeyButton"></param>
+    /// <returns></returns>
+    public string AssignString(string KeyString, Button KeyButton)
+    {
+        if(KeyString == null)
+        {
+            return KeyString = KeyButton.GetComponentInChildren<Text>().text.ToString();
+        }
+        else
+        {
+            return KeyString = PlayerPrefs.GetString(KeyButton.name.ToString());
+        }
+    }
+
+    /// <summary>
+    /// Metodo contenitore di tutte le assegnazioni di AssignString
+    /// </summary>
+    public void AssignStringContainer()
+    {
+        StringKeyUp = AssignString(StringKeyUp, KeyUp);
+        StringKeyDown = AssignString(StringKeyDown, KeyDown);
+        StringKeyLeft = AssignString(StringKeyLeft, KeyLeft);
+        StringKeyRight = AssignString(StringKeyRight, KeyRight);
+        StringKeyDash = AssignString(StringKeyDash, KeyDash);
+        StringKeyPossession = AssignString(StringKeyPossession, KeyPossession);
+        StringKeyLightAttack = AssignString(StringKeyLightAttack, KeyLightAttack);
+        StringKeyHeavyAttack = AssignString(StringKeyHeavyAttack, KeyHeavyAttack);
+        StringKeySpecialAttack = AssignString(StringKeySpecialAttack, KeySpecialAttack);
+    }
+
+    public void GetStringKeyPrefs()
+    {
+        StringKeyUp = PlayerPrefs.GetString(KeyUp.name.ToString());
+        StringKeyDown = PlayerPrefs.GetString(KeyDown.name.ToString());
+        StringKeyLeft = PlayerPrefs.GetString(KeyLeft.name.ToString());
+        StringKeyRight = PlayerPrefs.GetString(KeyRight.name.ToString());
+        StringKeyDash = PlayerPrefs.GetString(KeyDash.name.ToString());
+        StringKeyPossession = PlayerPrefs.GetString(KeyPossession.name.ToString());
+        StringKeyLightAttack = PlayerPrefs.GetString(KeyLightAttack.name.ToString());
+        StringKeyHeavyAttack = PlayerPrefs.GetString(KeyHeavyAttack.name.ToString());
+        StringKeySpecialAttack = PlayerPrefs.GetString(KeySpecialAttack.name.ToString());
+    }
+}
