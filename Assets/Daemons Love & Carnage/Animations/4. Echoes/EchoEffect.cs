921a13c3457b7dd58f5ec33c6b5cbc63fe8c5671
Andrea Facchinetti <facchiandrea1510@gmail.com>
2021-06-16 22:07:20 +0200

    Borious and Fat new dash effect

---
+++ Assets/Daemons Love & Carnage/Animations/4. Echoes/EchoEffect.cs
@@ -0,0 +1,31 @@
+﻿using SwordGame;
+using UnityEngine;
+public class EchoEffect : MonoBehaviour
+{
+    private float timeBetweenSpawn;
+    public float StartTimeBetweenSpawn;
+
+    public GameObject echo;
+    private PSMController PSMController;
+
+    private void Awake()
+    {
+        PSMController = this.gameObject.GetComponent<PSMController>();
+    }
+    private void Update()
+    {
+        if (PSMController.TimerDash <= PSMController.LimitTimerDash && PSMController.TimerDash != 0)
+        {
+            if (timeBetweenSpawn <= 0)
+            {
+                Instantiate(echo, transform.position, transform.rotation);
+                timeBetweenSpawn = StartTimeBetweenSpawn;
+            }
+            else
+            {
+                timeBetweenSpawn -= Time.deltaTime;
+            }
+
+        }
+    }
+}
