cd7d31a328ad319caaa8bd88d01879681a744c81
Andrea Facchinetti <facchiandrea1510@gmail.com>
2021-06-26 19:02:52 +0200

    Now the player sprite will change based on the current character in the boss dialogue

---
+++ Assets/BossDialogueImageSwitch.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class BossDialogueImageSwitch : MonoBehaviour
+{
+    public Image PlayerBossDialogue;
+    public Image ObscuredPlayerBossDialogue;
+
+    public Sprite FatKnightPlayerSprite;
+    public Sprite BoriousKnightPlayerSprite;
+    public Sprite BabushkaPlayerSprite;
+    public Sprite ThiefPlayerSprite;
+
+    void Update()
+    {
+        SubstitutePlayerImageBossDialogue();
+        SubstituteObscuredPlayerImageBossDialogue();
+    }
+
+
+    public void SubstitutePlayerImageBossDialogue()
+    {
+        switch (Possession.TypologyPlayer)
+        {
+            case SwordGame.TypePlayer.FatKnight:
+                PlayerBossDialogue.sprite = FatKnightPlayerSprite;
+                break;
+            case SwordGame.TypePlayer.BoriousKnight:
+                PlayerBossDialogue.sprite = BoriousKnightPlayerSprite;
+                break;
+            case SwordGame.TypePlayer.Babushka:
+                PlayerBossDialogue.sprite = BabushkaPlayerSprite;
+                break;
+            case SwordGame.TypePlayer.Thief:
+                PlayerBossDialogue.sprite = ThiefPlayerSprite;
+                break;
+            default:
+                break;
+        }
+    }
+    public void SubstituteObscuredPlayerImageBossDialogue()
+    {
+        switch (Possession.TypologyPlayer)
+        {
+            case SwordGame.TypePlayer.FatKnight:
+                ObscuredPlayerBossDialogue.sprite = FatKnightPlayerSprite;
+                break;
+            case SwordGame.TypePlayer.BoriousKnight:
+                ObscuredPlayerBossDialogue.sprite = BoriousKnightPlayerSprite;
+                break;
+            case SwordGame.TypePlayer.Babushka:
+                ObscuredPlayerBossDialogue.sprite = BabushkaPlayerSprite;
+                break;
+            case SwordGame.TypePlayer.Thief:
+                ObscuredPlayerBossDialogue.sprite = ThiefPlayerSprite;
+                break;
+            default:
+                break;
+        }
+    }
+
+}
