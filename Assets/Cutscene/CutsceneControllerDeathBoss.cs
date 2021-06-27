18a1cff5676a41b385ae43ee5e29ec9e59ea1497
leolaz99 <leolaz99@gmail.com>
2021-06-27 19:08:04 +0200

    added dialogue

--- Assets/Cutscene/CutsceneControllerDeathBoss.cs
+++ Assets/Cutscene/CutsceneControllerDeathBoss.cs
@@ -1,5 +1,13 @@
 ﻿using UnityEngine;
+using System.Collections;
 
 public class CutsceneControllerDeathBoss : MonoBehaviour
 {
+    public Dialogue dialogue;
+
+    public IEnumerator TriggerDialogue(float time)
+    {
+        yield return new WaitForSeconds(time);
+        DialogueManagerDeathBoss.instance.StartDialogue(dialogue);
+    }
 }
