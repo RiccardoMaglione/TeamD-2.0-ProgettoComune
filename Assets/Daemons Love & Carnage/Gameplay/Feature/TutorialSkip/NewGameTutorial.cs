6643ea692cb21528f2180e28dabd88f9975a01f1
Maglione Riccardo <riccardo99maglione@gmail.com>
2021-07-01 18:19:00 +0200

    Added reset tutorial skip in new game button

---
+++ Assets/Daemons Love & Carnage/Gameplay/Feature/TutorialSkip/NewGameTutorial.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewGameTutorial : MonoBehaviour
+{
+    public void ResetTutorial()
+    {
+        PlayerPrefs.SetInt("TutorialSkip", 0);
+    }
+}
