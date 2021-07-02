70b5114211b0598a26e81baa5034a4759d643af8
Maglione Riccardo <riccardo99maglione@gmail.com>
2021-07-02 15:23:13 +0200

    Added reset checkpoin in new game

---
+++ Assets/Daemons Love & Carnage/Gameplay/Feature/Checkpoint/CheckpointNewGame.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SwordGame
+{
+    public class CheckpointNewGame : MonoBehaviour
+    {
+        public void CheckNewGame()
+        {
+            print(PlayerPrefs.GetInt("IDCheckpoint", -1));
+            print(CheckpointManager.ContinueGame);
+        }
+    }
+}
