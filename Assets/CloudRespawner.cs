18416ab3b29e5856f37508db534345a5c054c915
Andrea Facchinetti <facchiandrea1510@gmail.com>
2021-07-03 19:32:43 +0200

    Clouds ok

---
+++ Assets/CloudRespawner.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class CloudRespawner : MonoBehaviour
+{
+    [SerializeField] Vector3 spawningPoint;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Cloud")
+        {
+            collision.transform.position = new Vector3(spawningPoint.x, collision.transform.position.y, collision.transform.position.z);
+        }
+    }
+}
