7ba6e1148c0689e1b596ac15e359f87cb6fdd8de
Andrea Facchinetti <facchiandrea1510@gmail.com>
2021-07-03 17:17:01 +0200

    Graphic fixes, switched skull inmages, switched placeholder images in play screen

---
+++ Assets/CloudMovement.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class CloudMovement : MonoBehaviour
+{
+    [SerializeField] float speed;
+    Vector3 startPos;
+    private void Start()
+    {
+        startPos = new Vector3(transform.position.x, transform.position.y, transform.position.z);
+    }
+    void Update()
+    {
+        transform.Translate(Vector3.right * speed * Time.deltaTime);
+
+        if (transform.position.x >= startPos.x + 80)
+        {
+            transform.position = startPos;
+        }
+
+
+    }
+}
