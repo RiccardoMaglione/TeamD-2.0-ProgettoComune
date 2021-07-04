06cfa366ae5d2092d9ebe3490fdbbd74a6680fbc
Maglione Riccardo <riccardo99maglione@gmail.com>
2021-07-04 16:51:42 +0200

    Modify local scale when hover level 3 button

---
+++ Assets/Daemons Love & Carnage/Gameplay/Feature/KeyBind/LevelButtonUI.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LevelButtonUI : MonoBehaviour
+{
+    public Sprite LevelButtonNormal;
+    public Sprite LevelButtonHover;
+
+    public void HoverSkull()
+    {
+        GetComponent<Image>().sprite = LevelButtonHover;
+        GetComponent<RectTransform>().localScale = new Vector3(4.3057f, 4.3057f, 4.3057f);
+    }
+    public void DeHoverSkull()
+    {
+        GetComponent<Image>().sprite = LevelButtonNormal;
+        GetComponent<RectTransform>().localScale = new Vector3(3.3057f, 3.3057f, 3.3057f);
+    }
+}
