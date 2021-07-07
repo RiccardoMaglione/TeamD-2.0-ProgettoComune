1b0c6738516756e1382488db6c2e4205973ae1f4
Andrea Facchinetti <facchiandrea1510@gmail.com>
2021-07-07 17:10:56 +0200

    Fix dashStopper

--- Assets/Daemons Love & Carnage/Scripts/DialogueSystem/DialogueTrigger.cs
+++ Assets/Daemons Love & Carnage/Scripts/DialogueSystem/DialogueTrigger.cs
@@ -3,12 +3,17 @@
 public class DialogueTrigger : MonoBehaviour
 {
     [SerializeField] Animator animator;
+    public static bool bossDialogueTrigger = false;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
+
         if (collision.gameObject.CompareTag("Player"))
+        {
+            bossDialogueTrigger = true;
             animator.SetTrigger("EnterTrigger");
-        
+        }
+
     }
 
     private void OnTriggerExit2D(Collider2D collision)
