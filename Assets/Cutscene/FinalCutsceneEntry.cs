d8fbd2ec011172a801e10afd85a43b469fb21281
leolaz99 <leolaz99@gmail.com>
2021-07-07 18:02:03 +0200

    added fade out on boss final cutscene

---
+++ Assets/Cutscene/FinalCutsceneEntry.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FinalCutsceneEntry : MonoBehaviour
+{
+    private void Awake()
+    {
+        AudioManager.instance.FadeOut("BossOST");
+    }
+}
