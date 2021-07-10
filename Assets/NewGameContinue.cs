db87c9312ce1d98926a6fecbe9b48dd325276054
Maglione Riccardo <riccardo99maglione@gmail.com>
2021-07-10 23:25:22 +0200

    Created logic for confirm new game

---
+++ Assets/NewGameContinue.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NewGameContinue : MonoBehaviour
+{
+    public Button NewGame;
+    public Button Continue;
+
+    public GameObject PanelConfirmNewGame;
+
+    void Update()
+    {
+        if(PlayerPrefs.GetInt("NewGameFirst") == 0)
+        {
+            Continue.interactable = false;
+        }
+        else
+        {
+            Continue.interactable = true;
+        }
+    }
+
+    public void ConfirmNewGame()
+    {
+        if (PlayerPrefs.GetInt("NewGameFirst") == 1)
+        {
+            PanelConfirmNewGame.SetActive(true);
+        }
+    }
+
+    public void SetNewGame()
+    {
+        PlayerPrefs.SetInt("NewGameFirst", 1);
+    }
+    public void ResetSetNewGame()
+    {
+        PlayerPrefs.SetInt("NewGameFirst", 0);
+    }
+}
