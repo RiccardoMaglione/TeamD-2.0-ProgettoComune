33c3c725f0c3d1707a5135c11aa76c65b0319bb2
Andrea Facchinetti <facchiandrea1510@gmail.com>
2021-07-10 21:37:45 +0200

    Random speed of plants

---
+++ Assets/RandomStartAnimation.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+public class RandomStartAnimation : MonoBehaviour
+{
+    private void Start()
+    {
+        GetComponent<Animator>().speed = Random.Range(0.6f, 1.4f);
+    }
+}
