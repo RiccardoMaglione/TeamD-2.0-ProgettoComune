ead822e76b82fed1cf14b5ea659de89356bcac1b
Maglione Riccardo <riccardo99maglione@gmail.com>
2021-07-11 13:57:07 +0200

    Added feedback color block in option menu

--- Assets/Daemons Love & Carnage/Gameplay/Feature/KeyBind/ChangeColorSelected.cs
+++ Assets/Daemons Love & Carnage/Gameplay/Feature/KeyBind/ChangeColorSelected.cs
@@ -12,27 +12,64 @@ public class ChangeColorSelected : MonoBehaviour
     ColorBlock ControllerSelectedColorBlock;
 
     [HideInInspector] public Button ThisButton;
-    
+    [HideInInspector] public Toggle ThisToggle;
+    [HideInInspector] public Slider ThisSlider;
+
     private void Awake()
     {
-        ThisButton = gameObject.GetComponent<Button>();
+        if(gameObject.GetComponent<Button>() != null)
+        {
+            ThisButton = gameObject.GetComponent<Button>();
 
-        NormalSelectedColorBlock = ThisButton.colors;
-        NormalSelectedColorBlock.selectedColor = NormalSelectedColor;
+            NormalSelectedColorBlock = ThisButton.colors;
+            NormalSelectedColorBlock.selectedColor = NormalSelectedColor;
 
-        ControllerSelectedColorBlock = ThisButton.colors;
-        ControllerSelectedColorBlock.selectedColor = ControllerSelectedColor;
+            ControllerSelectedColorBlock = ThisButton.colors;
+            ControllerSelectedColorBlock.selectedColor = ControllerSelectedColor;
+        }
+
+        if(gameObject.GetComponent<Toggle>() != null)
+        {
+            ThisToggle = gameObject.GetComponent<Toggle>();
+
+            NormalSelectedColorBlock = ThisToggle.colors;
+            NormalSelectedColorBlock.selectedColor = NormalSelectedColor;
+
+            ControllerSelectedColorBlock = ThisToggle.colors;
+            ControllerSelectedColorBlock.selectedColor = ControllerSelectedColor;
+        }
+
+        if(gameObject.GetComponent<Slider>() != null)
+        {
+            ThisSlider = gameObject.GetComponent<Slider>();
+
+            NormalSelectedColorBlock = ThisSlider.colors;
+            NormalSelectedColorBlock.selectedColor = NormalSelectedColor;
+
+            ControllerSelectedColorBlock = ThisSlider.colors;
+            ControllerSelectedColorBlock.selectedColor = ControllerSelectedColor;
+        }
     }
 
     private void Update()
     {
         if (CheckInput.Controller == false)
         {
-            ThisButton.colors = NormalSelectedColorBlock;
+            if(ThisButton != null)
+                ThisButton.colors = NormalSelectedColorBlock;
+            if(ThisToggle != null)
+                ThisToggle.colors = NormalSelectedColorBlock;
+            if(ThisSlider != null)
+                ThisSlider.colors = NormalSelectedColorBlock;
         }
         if (CheckInput.Controller == true)
         {
-            ThisButton.colors = ControllerSelectedColorBlock;
+            if (ThisButton != null)
+                ThisButton.colors = ControllerSelectedColorBlock;
+            if (ThisToggle != null)
+                ThisToggle.colors = ControllerSelectedColorBlock;
+            if (ThisSlider != null)
+                ThisSlider.colors = ControllerSelectedColorBlock;
         }
     }
 }
