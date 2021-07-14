b945d47999012f46be219b7b519933cc70ed1b4f
Andrea Facchinetti <facchiandrea1510@gmail.com>
2021-07-14 15:55:00 +0200

    Nerf fat knight damage reduction (0.3->0.4) + fix stagger boss

--- Assets/Daemons Love & Carnage/Scripts/Boss Script/HumanoidBoss.cs
+++ Assets/Daemons Love & Carnage/Scripts/Boss Script/HumanoidBoss.cs
@@ -8,24 +8,28 @@ public class HumanoidBoss : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        int rand = Random.Range(1, 3);
-        if (rand == 1)
-            animator.SetTrigger("GoToStagger1");
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            int rand = Random.Range(1, 3);
+            if (rand == 1)
+                animator.SetTrigger("GoToStagger1");
 
-        if (rand == 2)
-            animator.SetTrigger("GoToStagger2");
+            if (rand == 2)
+                animator.SetTrigger("GoToStagger2");
 
-        GetComponent<ButtonMashCounter>().attack++;
-        GameObject tempHitEffect = Instantiate(hit.gameObject, transform.position, Quaternion.identity);
-        tempHitEffect.GetComponent<ParticleSystem>().Play();
+            GetComponent<ButtonMashCounter>().attack++;
+            GameObject tempHitEffect = Instantiate(hit.gameObject, transform.position, Quaternion.identity);
+            tempHitEffect.GetComponent<ParticleSystem>().Play();
 
-        Destroy(tempHitEffect, 0.5f);
+            Destroy(tempHitEffect, 0.5f);
 
-        GameObject tempHitEffect2 = Instantiate(blood.gameObject, transform.position, Quaternion.identity);
-        tempHitEffect2.GetComponent<ParticleSystem>().Play();
+            GameObject tempHitEffect2 = Instantiate(blood.gameObject, transform.position, Quaternion.identity);
+            tempHitEffect2.GetComponent<ParticleSystem>().Play();
 
-        Destroy(tempHitEffect2, 0.5f);
+            Destroy(tempHitEffect2, 0.5f);
 
-        AudioManager.instance.Play("Sfx_boss_imp_hit");
+            AudioManager.instance.Play("Sfx_boss_imp_hit");
+
+        }
     }
 }
