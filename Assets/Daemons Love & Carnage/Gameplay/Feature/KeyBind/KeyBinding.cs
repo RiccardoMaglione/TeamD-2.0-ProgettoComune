b3260d8bbd535c45716f238f55e3202c29b03f10
Maglione Riccardo <riccardo99maglione@gmail.com>
2021-07-18 21:13:40 +0200

    Added KeyValue != null in SetAxisSign

--- Assets/Daemons Love & Carnage/Gameplay/Feature/KeyBind/KeyBinding.cs
+++ Assets/Daemons Love & Carnage/Gameplay/Feature/KeyBind/KeyBinding.cs
@@ -700,12 +700,12 @@ public class KeyBinding : KeyVar
 
     public bool SetAxisSign(string KeyValue)
     {
-        if (KeyValue == "Left Right Joystick" || KeyValue == "Left Up Joystick" || KeyValue == "DPad Right" || KeyValue == "DPad Up" || KeyValue == "DPad Right" || KeyValue == "Left Trigger" || KeyValue == "Right Trigger" || KeyValue == "Right HRJ" || KeyValue == "Up VRJ" || KeyValue == "PS DPad right" || KeyValue == "PS DPad Up" || KeyValue == "PS Right HRJ" || KeyValue == "PS Up VRJ")
+        if (KeyValue != null && (KeyValue == "Left Right Joystick" || KeyValue == "Left Up Joystick" || KeyValue == "DPad Right" || KeyValue == "DPad Up" || KeyValue == "DPad Right" || KeyValue == "Left Trigger" || KeyValue == "Right Trigger" || KeyValue == "Right HRJ" || KeyValue == "Up VRJ" || KeyValue == "PS DPad right" || KeyValue == "PS DPad Up" || KeyValue == "PS Right HRJ" || KeyValue == "PS Up VRJ"))
         {
             //Debug.Log("Maggiore");
             return Input.GetAxisRaw(SetKeyBindControllerAxis(KeyValue)) > 0.5f;
         }
-        if (KeyValue == "Left Left Joystick" || KeyValue == "Left Down Joystick" || KeyValue == "DPad Left" || KeyValue == "DPad Down" || KeyValue == "DPad Left" || KeyValue == "Left HRJ" || KeyValue == "Down VRJ" || KeyValue == "PS DPad Left" || KeyValue == "PS DPad Down" || KeyValue == "PS Left HRJ" || KeyValue == "PS Down VRJ")
+        if (KeyValue != null && (KeyValue == "Left Left Joystick" || KeyValue == "Left Down Joystick" || KeyValue == "DPad Left" || KeyValue == "DPad Down" || KeyValue == "DPad Left" || KeyValue == "Left HRJ" || KeyValue == "Down VRJ" || KeyValue == "PS DPad Left" || KeyValue == "PS DPad Down" || KeyValue == "PS Left HRJ" || KeyValue == "PS Down VRJ"))
         {
             //Debug.Log("Minore");
             return Input.GetAxisRaw(SetKeyBindControllerAxis(KeyValue)) < -0.5f;
