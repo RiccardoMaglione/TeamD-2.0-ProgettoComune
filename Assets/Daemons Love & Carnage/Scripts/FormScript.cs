1195dd24edc9a6a1f4a2fadb6dcf44b95a0774cd
Andrea Facchinetti <facchiandrea1510@gmail.com>
2021-07-18 23:57:50 +0200

    Rate link added

--- Assets/Daemons Love & Carnage/Scripts/FormScript.cs
+++ Assets/Daemons Love & Carnage/Scripts/FormScript.cs
@@ -6,4 +6,10 @@ public class FormScript : MonoBehaviour
     {
         Application.OpenURL("https://docs.google.com/forms/d/1quVvgK68PzNSQjJGeist9_Zfuuy_wnTO5NlLCAozkEo/edit?usp=sharing");
     }
+    public void OpenRate()
+    {
+        Application.OpenURL("https://eventhorizonschool.itch.io/daemons-love-and-carnage/rate?source=game");
+    }
+
+
 }
