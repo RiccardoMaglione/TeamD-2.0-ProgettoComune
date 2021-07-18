879e2ad3ec9546c84a8cb3f074ca1c53fb85ee1c
leolaz99 <leolaz99@gmail.com>
2021-07-19 01:23:00 +0200

    added playerprefs reset

--- Assets/Menu/Scripts/TargetFpsManager.cs
+++ Assets/Menu/Scripts/TargetFpsManager.cs
@@ -6,5 +6,11 @@ public class TargetFpsManager : MonoBehaviour
     {
         QualitySettings.vSyncCount = 0;
         Application.targetFrameRate = 60;
+
+        if (PlayerPrefs.GetInt("StartGame", 0) == 0)
+        {
+            PlayerPrefs.DeleteAll();
+            PlayerPrefs.SetInt("StartGame", 1);
+        }   
     }
 }
